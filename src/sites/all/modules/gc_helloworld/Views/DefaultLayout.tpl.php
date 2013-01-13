01160fe7cfd834dc68f5780839623443fdac3c2d
Dave <damanshia@ebay.com>
2013-01-13 14:43:08 +0000

    new readme, minor js changes

--- src/sites/all/modules/gc_helloworld/Views/DefaultLayout.tpl.php
+++ src/sites/all/modules/gc_helloworld/Views/DefaultLayout.tpl.php
@@ -4,8 +4,6 @@
         <div class="pageRow">
             <div id="logo">
             </div>
-
-
         </div> <!-- end pageRow div-->
 
         <div class="pageRow">
@@ -14,7 +12,7 @@
 
         <div class="pageRow">
             <p>
-                Brought to you by eBay UK
+                Brought to you by Golden Contact Computing
             </p>
         </div> <!-- end pageRow div-->
 
