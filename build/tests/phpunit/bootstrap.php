dd87ac2276a255a49ede5e2f1f73efe30f70a957
Golden Dave <golden@goldenballs.(none)>
2013-02-03 15:40:59 +0000

    for jenkins

--- build/tests/phpunit/bootstrap.php
+++ build/tests/phpunit/bootstrap.php
@@ -7,7 +7,7 @@ class bootStrapForTests {
 
     public function launch() {
         $basePath = str_replace('build/tests/phpunit', "", dirname(__FILE__));
-        require_once ($basePath."/src/sites/all/modules/gc_helloworld/AutoLoad.php");
+        require_once ($basePath."src/sites/all/modules/gc_rpsgame/AutoLoad.php");
         $autoLoader = new \Core\autoLoader();
         $autoLoader->launch();
     }
